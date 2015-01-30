ee54311b7166d504f6abeadd755b77484e4e8f18
Peter Rosser <peter@booyami.com>
2015-01-30 00:20:29 -0800

    Adding non-HttpSessionState constructors and cleaning up [de]serialization for Session object.
    Session now allows callers to subscribe to the Saved event so they can react when the session is changed.

--- src/Maestrano/Sso/Session.cs
+++ src/Maestrano/Sso/Session.cs
@@ -1,42 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.SessionState;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using RestSharp;
+using System;
+using System.Web.SessionState;
 
 namespace Maestrano.Sso
 {
-    public class Session
+    [Flags]
+    public enum SessionValidationSteps
     {
-
-        public string Uid { get; set; }
-        public string GroupUid { get; set; }
-        public DateTime Recheck { get; set; }
-        public string SessionToken { get; set; }
-        public HttpSessionState HttpSession { get; set; }
+        /// <summary>
+        /// No validation will be performed.
+        /// </summary>
+        None = 0,
 
         /// <summary>
-        /// Contructor retrieving maestrano session from
-        /// http session
+        /// Validation will succeed immediately if there HttpSessionState is null
+        /// </summary>
+        ValidIfNoHttpSessionState = 0x1,
+
+        /// <summary>
+        /// Validation will fail if HttpSessionState is null.
+        /// </summary>
+        RequireHttpSessionState = 0x2,
+
+        /// <summary>
+        /// Validation will fail if there are no event listeners subscribed to the Saved event.
+        /// </summary>
+        RequireEventListener = 0x4,
+
+        /// <summary>
+        /// IsValid will call PerformRemoteCheck (if required), and validation will fail if that call fails.
+        /// </summary>
+        PerformRemoteCheck = 0x5,
+
+        Default = PerformRemoteCheck,
+    }
+
+    public class Session
+    {
+        private const string HttpSessionStateKey = "maestrano";
+
+        /// <summary>
+        /// Create an empty Maestrano SSO Session
+        /// </summary>
+        public Session()
+        {
+        }
+
+        /// <summary>
+        /// Create a Maestrano SSO session that uses an HttpSessionState object as its backing store
         /// </summary>
         /// <param name="httpSessionObj"></param>
         public Session(HttpSessionState httpSessionObj)
+            : this(httpSessionObj, null)
+        {
+        }
+
+        /// <summary>
+        /// Create a Maestrano SSO session that uses an HttpSessionState object as its backing store and
+        /// is initialized with the data from a User object
+        /// </summary>
+        /// <param name="httpSessionObj">HttpSessionState object to use as the backing store for the session</param>
+        /// <param name="user">User object that contains the initialization data for the Session</param>
+        /// <remarks>Updates to the User object will not be automatically reflected by the Session</remarks>
+        public Session(HttpSessionState httpSessionObj, User user)
+            : this(httpSessionObj == null ? null : httpSessionObj[HttpSessionStateKey] as string, user)
         {
             HttpSession = httpSessionObj;
+        }
 
-            if (HttpSession != null && HttpSession["maestrano"] != null)
+        /// <summary>
+        /// Create a Maestrano SSO session that is initialized with the data from a User object
+        /// </summary>
+        /// <param name="user">User object that contains the initialization data for the Session</param>
+        /// <remarks>Updates to the User object will not be automatically reflected by the Session</remarks>
+        public Session(User user)
+            : this((string)null, user)
+        {
+        }
+
+        private Session(string encodedMnoSession, User user)
+        {
+            if (encodedMnoSession != null)
             {
                 var enc = System.Text.Encoding.UTF8;
                 JObject sessionObject = new JObject();
-                try {
-                    string decryptedMnoSession = enc.GetString(Convert.FromBase64String(HttpSession["maestrano"].ToString()));
-                    sessionObject = JObject.Parse(decryptedMnoSession);
+                try
+                {
+                    string decoded = enc.GetString(Convert.FromBase64String(encodedMnoSession));
+                    sessionObject = JObject.Parse(decoded);
                 }
-                catch (Exception){ }
-                
+                catch (Exception) { }
+
                 // Assign attributes
                 Uid = sessionObject.Value<String>("uid");
                 GroupUid = sessionObject.Value<String>("group_uid");
@@ -48,21 +103,10 @@ namespace Maestrano.Sso
                     Recheck = sessionObject.Value<DateTime>("session_recheck");
                 }
                 catch (Exception) { }
-                
-
-                if (Recheck == null)
-                    Recheck = DateTime.UtcNow.AddMinutes(-1);
             }
-        }
 
-        /// <summary>
-        /// Contructor retrieving maestrano session from user
-        /// </summary>
-        /// <param name="httpSessionObj"></param>
-        /// <param name="user"></param>
-        public Session(HttpSessionState httpSessionObj, User user)
-        {
-            HttpSession = httpSessionObj;
+            if (Recheck == null)
+                Recheck = DateTime.UtcNow.AddMinutes(-1);
 
             if (user != null)
             {
@@ -73,11 +117,30 @@ namespace Maestrano.Sso
             }
         }
 
+        public event EventHandler Saved;
+
+        public string GroupUid { get; set; }
+
+        public HttpSessionState HttpSession { get; set; }
+
+        public DateTime Recheck { get; set; }
+
+        public string SessionToken { get; set; }
+
+        public string Uid { get; set; }
+
+        public static Session FromEncodedString(string encodedMnoSession)
+        {
+            Session session = new Session(encodedMnoSession, null);
+            return session;
+        }
+
         /// <summary>
         /// Returns whether the session needs to be checked
-        /// remotely from maestrano or not
+        /// remotely from Maestrano or not
         /// </summary>
-        /// <returns></returns>
+        /// <returns>True if the current time is past the recheck time
+        /// or if any property is null. Otherwise, false.</returns>
         public Boolean isRemoteCheckRequired()
         {
             if (Uid != null && SessionToken != null && Recheck != null)
@@ -88,71 +151,88 @@ namespace Maestrano.Sso
         }
 
         /// <summary>
-        /// Check whether the remote maestrano session is still
-        /// valid
+        /// Return whether the session is valid or not. Performs a
+        /// remote check to Maestrano if required.
         /// </summary>
-        /// <param name="client"></param>
-        /// <returns></returns>
-        public Boolean PerformRemoteCheck(RestClient client)
+        /// <param name="client">RestClient to use for Remote validation</param>
+        /// <returns>True if the Session should be treated as valid, otherwise false.</returns>
+        public Boolean IsValid()
         {
-            if (Uid != null && SessionToken != null && Uid.Length > 0 && SessionToken.Length > 0)
+            return IsValid(null, SessionValidationSteps.Default);
+        }
+
+        /// <summary>
+        /// Return whether the session is valid or not. Performs a
+        /// remote check to Maestrano if required.
+        /// </summary>
+        /// <param name="steps">Validation steps to perform</param>
+        /// <returns>True if the Session should be treated as valid, otherwise false.</returns>
+        public Boolean IsValid(SessionValidationSteps steps)
+        {
+            return IsValid(null, steps);
+        }
+
+        /// <summary>
+        /// Return whether the session is valid or not. Performs a
+        /// remote check to Maestrano if required.
+        /// </summary>
+        /// <param name="client">RestClient to use for Remote validation</param>
+        /// <returns>True if the Session should be treated as valid, otherwise false.</returns>
+        public Boolean IsValid(RestClient client)
+        {
+            return IsValid(client, SessionValidationSteps.Default);
+        }
+
+        /// <summary>
+        /// Return whether the session is valid or not. Performs a
+        /// remote check to Maestrano if required.
+        /// </summary>
+        /// <param name="client">RestClient to use for Remote validation</param>
+        /// <param name="steps">Validation steps to perform</param>
+        /// <returns>True if the Session should be treated as valid, otherwise false.</returns>
+        public Boolean IsValid(RestClient client, SessionValidationSteps steps)
+        {
+            if (steps == SessionValidationSteps.None)
             {
-                // Prepare request
-                var request = new RestRequest("api/v1/auth/saml/{id}", Method.GET);
-                request.AddUrlSegment("id", Uid);
-                request.AddParameter("session", SessionToken);
-                JObject resp = new JObject();
-                try {
-                    resp = JObject.Parse(client.Execute(request).Content);
-                }
-                catch (Exception) { }
-
-                bool valid = Convert.ToBoolean(resp.Value<String>("valid"));
-                string dateStr = resp.Value<String>("recheck");
-                if ( valid && dateStr != null && dateStr.Length > 0)
-                {
-                    Recheck = DateTime.Parse(dateStr);
-                    return true;
-                }
+                throw new ArgumentOutOfRangeException("steps", "IsValid should be called if you are not going to do any validation!");
             }
-            return false;
-        }
 
-        /// <summary>
-        /// Check whether the remote maestrano session is still
-        /// valid
-        /// </summary>
-        /// <returns></returns>
-        public Boolean PerformRemoteCheck()
-        {
-            var client = new RestClient(MnoHelper.Sso.Idp);
-            return PerformRemoteCheck(client);
-        }
+            if ((steps & SessionValidationSteps.ValidIfNoHttpSessionState) != SessionValidationSteps.ValidIfNoHttpSessionState)
+            {
+                throw new ArgumentOutOfRangeException("steps", "ValidIfNoHttpSessionState must be the only step if it is specified.");
+            }
 
-        /// <summary>
-        /// Return wether the session is valid or not. Perform
-        /// remote check to maestrano if recheck is overdue.
-        /// </summary>
-        /// <param name="client"></param>
-        /// <param name="ifSession"></param>
-        /// <returns></returns>
-        public Boolean IsValid(RestClient client, Boolean ifSession = false)
-        {
-            // Return true automatically if SLO is disabled
+            // Return true automatically if Single Logout (SLO) is disabled
             if (!MnoHelper.Sso.SloEnabled)
                 return true;
 
             // Return true if maestrano session not set
-            // and ifSession option enabled
-            if (ifSession && (HttpSession == null || HttpSession["maestrano"] == null))
+            // and only the HttpSessionState step is requested
+            if (steps == SessionValidationSteps.ValidIfNoHttpSessionState && (HttpSession == null || HttpSession["maestrano"] == null))
                 return true;
 
-            // Return false if HttpSession is nil
-            if (HttpSession == null)
-                return false;
-
-            if (isRemoteCheckRequired())
+            // Perform local validation
+            if ((steps & (SessionValidationSteps.RequireEventListener | SessionValidationSteps.RequireHttpSessionState)) == steps)
             {
+                if (HttpSession == null && Saved == null)
+                    return false;
+            }
+            else
+            {
+                if (steps.HasFlag(SessionValidationSteps.RequireHttpSessionState) && HttpSession == null)
+                    return false;
+
+                if (steps.HasFlag(SessionValidationSteps.RequireEventListener) && Saved == null)
+                    return false;
+            }
+
+            if (steps.HasFlag(SessionValidationSteps.PerformRemoteCheck) && isRemoteCheckRequired())
+            {
+                if (client == null)
+                {
+                    client = new RestClient(MnoHelper.Sso.Idp);
+                }
+
                 if (PerformRemoteCheck(client))
                 {
                     Save();
@@ -167,33 +247,103 @@ namespace Maestrano.Sso
         }
 
         /// <summary>
-        /// Return wether the session is valid or not. Perform
-        /// remote check to maestrano if recheck is overdue.
+        /// Obsolete. Return whether the session is valid or not. Performs a
+        /// remote check to Maestrano if required.
         /// </summary>
-        /// <param name="ifSession">If set to true then session return false ONLY if maestrano session exists and is invalid</param>
-        /// <returns></returns>
-        public Boolean IsValid(Boolean ifSession = false)
+        /// <param name="ifSession">If set to true then session returns
+        /// false ONLY if Maestrano session exists and is invalid</param>
+        /// <returns>True if the Session should be treated as valid, otherwise false.</returns>
+        [Obsolete("Use IsValid(SessionValidationTypes) instead")]
+        public Boolean IsValid(Boolean ifSession)
         {
             var client = new RestClient(MnoHelper.Sso.Idp);
-            return IsValid(client,ifSession);
+            return IsValid(client, ifSession ? SessionValidationSteps.RequireHttpSessionState : SessionValidationSteps.Default);
         }
 
         /// <summary>
-        /// Save the Maestrano session in
-        /// HTTP Session
+        /// Obsolete. Return whether the session is valid or not. Performs a
+        /// remote check to Maestrano if required.
+        /// </summary>
+        /// <param name="client">RestClient initialized to point to
+        /// a Maestrano API environment.</param>
+        /// <param name="ifSession"></param>
+        /// <returns></returns>
+        [Obsolete("Use IsValid(RestClient, SessionValidationTypes) instead")]
+        public Boolean IsValid(RestClient client, Boolean ifSession)
+        {
+            return IsValid(client, ifSession ? SessionValidationSteps.RequireHttpSessionState : SessionValidationSteps.Default);
+        }
+
+        /// <summary>
+        /// Check whether the remote Maestrano session is still valid
+        /// </summary>
+        /// <param name="client">RestClient initialized to point to a Maestrano API environment.</param>
+        /// <returns>True if the Maestrano API successfully validated this Session, otherwise false.</returns>
+        public Boolean PerformRemoteCheck(RestClient client)
+        {
+            if (Uid != null && SessionToken != null && Uid.Length > 0 && SessionToken.Length > 0)
+            {
+                // Prepare request
+                var request = new RestRequest("api/v1/auth/saml/{id}", Method.GET);
+                request.AddUrlSegment("id", Uid);
+                request.AddParameter("session", SessionToken);
+                JObject resp = new JObject();
+                try
+                {
+                    resp = JObject.Parse(client.Execute(request).Content);
+                }
+                catch (Exception) { }
+
+                bool valid = Convert.ToBoolean(resp.Value<String>("valid"));
+                string dateStr = resp.Value<String>("recheck");
+                if (valid && dateStr != null && dateStr.Length > 0)
+                {
+                    Recheck = DateTime.Parse(dateStr);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Check whether the remote Maestrano session is still valid.
+        /// </summary>
+        /// <returns></returns>
+        public Boolean PerformRemoteCheck()
+        {
+            var client = new RestClient(MnoHelper.Sso.Idp);
+            return PerformRemoteCheck(client);
+        }
+
+        /// <summary>
+        /// Save the Maestrano session in HTTP Session
         /// </summary>
         public void Save()
         {
-            var enc = System.Text.Encoding.UTF8;
-            JObject sessionObject = new JObject(
-                new JProperty("uid",Uid),
-                new JProperty("session",SessionToken),
-                new JProperty("session_recheck",Recheck.ToString("s")),
-                new JProperty("group_uid",GroupUid));
+            // notify subscribers that things have changed
+            if (Saved != null)
+            {
+                Saved(this, EventArgs.Empty);
+            }
 
-            // Finally store the maestrano session
-            HttpSession["maestrano"] = Convert.ToBase64String(enc.GetBytes(sessionObject.ToString()));
+            // if an HttpSession is being used, update it
+            if (HttpSession != null)
+            {
+                string encodedString = ToEncodedString();
+                HttpSession[HttpSessionStateKey] = encodedString;
+            }
         }
 
+        public string ToEncodedString()
+        {
+            var enc = System.Text.Encoding.UTF8;
+            JObject sessionObject = new JObject(
+                new JProperty("uid", Uid),
+                new JProperty("session", SessionToken),
+                new JProperty("session_recheck", Recheck.ToString("s")),
+                new JProperty("group_uid", GroupUid));
+
+            return Convert.ToBase64String(enc.GetBytes(sessionObject.ToString()));
+        }
     }
 }
